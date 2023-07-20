82447c9c7e9ec79fb407d06eec9ffb9f5d6c746b
laura-james <lollypopstar70@gmail.com>
2023-07-20 19:18:50 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -2,24 +2,49 @@ let sprite_sheet;
 let firework;
 
 function preload(){
-  firework = loadAnimation("Firework.png",{ frameSize: [256,256], frames: 30})
-  //mysheet = loadAnimation("myspritesheet.png",{ frameSize: [32,32], frames: 11})
+
   mysheet2 = loadAnimation("spritesheet2.png",{ frameSize: [100,100], frames: 16})
 }
 function setup() {
   new Canvas();
-
+  //world.gravity.y = 10;
+  monster = new Sprite();
+	monster.img = 'monster.png';
+	monster.diameter = 32;
+  msgbox = new Sprite(300,100,500,50,"static");
+  msgbox.color = "magenta";
+  msgbox.textSize = 14;
 }//end setup function
 
 function draw() {
   background(0);
 
+  secs = Math.floor(millis()/1000)
+  msgbox.text = secs;
+  if (secs >= 10){
+    msgbox.text = "GAME OVER";
+    msgbox.color = "cyan"
+  }
+  if (secs%10 == 0){
+    new Sprite(300,300,10)
+
+  }
+  
+  monster.speed = 5;
+  if (kb.pressing('up')) {
+		monster.direction = -90;
+	} else if (kb.pressing('down')) {
+		monster.direction = 90;
+	} else if (kb.pressing('left')) {
+		monster.direction = 180;
+	} else if (kb.pressing('right')) {
+		monster.direction = 0;
+	} else {
+	  monster.speed = 0;
+	}
+	
+	
 
 }//end draw function
-function mousePressed(){
-  let s = createSprite(mouseX, mouseY, 100)
-  s.addAnimation(mysheet2)
-  s.velocity.x = random(-5,5)
-  s.rotationSpeed = 2
-  s.life=200 //num frames to live for
-}
+
+
